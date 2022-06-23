e3d8f32ace4b0b2ddc69df3f8f513ced7ab5b3d9
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-06-23 18:08:15 +0600

    ema john server added with product collection

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjics.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emajohn").collection("product");
+
+    app.get("/product", async (req, res) => {
+      console.log("query", req.query);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = productCollection.find(query);
+      let products;
+      if (page || size) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+      res.send(products);
+    });
+
+    app.get("/prodouctCount", async (req, res) => {
+      const query = {};
+      const count = await productCollection.countDocuments(query);
+      res.send({ count });
+    });
+
+    // get product by keys using post
+    app.post("/productbykeys", async (req, res) => {
+      const keys = req.body;
+      const ids = keys.map((id) => ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome from Emajohn server");
+});
+
+app.listen(port, () => {
+  console.log("Running emajohn server on port:", port);
+});
+
+// p6TJek6aFSI8TW5I
+// emajohndb
